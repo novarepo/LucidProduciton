4487f53593960c89f01a6575049297d11ac0c7cc
illusionTBA <illusionztba@gmail.com>
2022-10-01 15:19:09 -0400

    arc

--- index.mjs
+++ index.mjs
@@ -1,46 +1,58 @@
-import createBareServer from '@tomphttp/bare-server-node';
-import http from 'http';
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+import createBareServer from "@tomphttp/bare-server-node";
+import http from "http";
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-import express from 'express';
+import express from "express";
 
 const httpServer = http.createServer();
 
 const expressServer = express();
 
-expressServer.use(express.static('public'))
+expressServer.use(express.static("public"));
 
-expressServer.get('/', (req, res) => {
-	res.sendFile('index.html');
+expressServer.use(
+  "/arc",
+  createProxyMiddleware({
+    target: "https://arc.io/widget.min.js#w55NMRME",
+    changeOrigin: true,
+    secure: true,
+    ws: true,
+  })
+);
+
+expressServer.get("/", (req, res) => {
+  res.sendFile("index.html");
 });
 
-
-const bareServer = createBareServer('/bear/', {
-	maintainer: {
-		email: 'webmaster@goastral.net',
-		website: 'https://github.com/AstralsService/Lucid',
-	},
+const bareServer = createBareServer("/bear/", {
+  maintainer: {
+    email: "webmaster@goastral.net",
+    website: "https://github.com/AstralsService/Lucid",
+  },
 });
 
-httpServer.on('request', (req, res) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeRequest(req, res);
-	} else {
-		expressServer(req, res);
-	}
+httpServer.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    expressServer(req, res);
+  }
 });
 
-httpServer.on('upgrade', (req, socket, head) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeUpgrade(req, socket, head);
-	} else {
-		socket.end();
-	}
+httpServer.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
 });
 
-httpServer.on('listening', () => {
-	console.log('HTTP server listening');
+httpServer.on("listening", () => {
+  console.log("HTTP server listening");
 });
 
 httpServer.listen({
-	port: 8080,
+  port: 8080,
 });
